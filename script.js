c9c57800d63a5bc0ac814352d27d35d8a5baccdf
AmithPictography <amithradarapu@gmail.com>
2025-08-10 21:25:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,26 +5,21 @@ document.addEventListener("DOMContentLoaded", () => {
         "photo2.jpg",
         "photo3.jpg"
     ];
-
     images.forEach(img => {
         let imgElement = document.createElement("img");
         imgElement.src = `images/${img}`;
         gallery.appendChild(imgElement);
-
         imgElement.addEventListener("click", () => {
             openLightbox(imgElement.src);
         });
     });
-
     const lightbox = document.getElementById("lightbox");
     const lightboxImg = document.getElementById("lightbox-img");
     const closeBtn = document.getElementById("close");
-
     function openLightbox(src) {
         lightbox.style.display = "flex";
         lightboxImg.src = src;
     }
-
     closeBtn.addEventListener("click", () => {
         lightbox.style.display = "none";
     });
